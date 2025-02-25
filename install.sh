ef102f4c767d481fd92b351830c5be4f1d0e4a8a
Jan Biernacik <jan@biernacik.org>
2025-02-25 13:30:31 +0100

    Fix

--- install.sh
+++ install.sh
@@ -6,39 +6,35 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 apt update && DEBIAN_FRONTEND=noninteractive apt upgrade -y
-DEBIAN_FRONTEND=noninteractive apt install gnupg curl sudo -y
-awk -F: '$3 >= 1000 && $3 < 65534 {print $1}' /etc/passwd | while read user; do
-    /sbin/usermod -aG sudo "$user"
-done
+DEBIAN_FRONTEND=noninteractive apt install gnupg curl laptop-detect -y
+mkdir -p /etc/dconf/profile
+mkdir -p /etc/dconf/db/local.d
 
-set +e
-mkdir -v /etc/dconf/profile
-mkdir -v /etc/dconf/db/local.d
-set -e
+DEBIAN_FRONTEND=noninteractive apt install gnome-core chromium- chromium-browser- epiphany-browser- gnome-www-browser- firefox- firefox-esr- gnome-software-plugin-deb- gnome-software-plugin-fwupd gnome-terminal -y
 
-DEBIAN_FRONTEND=noninteractive apt install gnome-core chromium- chromium-browser- epiphany-browser- gnome-www-browser- firefox- firefox-esr- gnome-software-plugin-deb- gnome-terminal -y
+(cd /tmp && curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg)
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+apt update
+
 set +e
 dpkg -i debuntu-meta.deb
 set -e
 DEBIAN_FRONTEND=noninteractive apt install -f -y
 
-DEBIAN_FRONTEND=noninteractive apt install tlp tlp-rdw -y
+laptop-detect && {
+  DEBIAN_FRONTEND=noninteractive apt install tlp tlp-rdw -y
+} || {
+  echo "Not a laptop, skipping tlp installation."
+}
 
-DEBIAN_FRONTEND=noninteractive apt install --install-recommends fonts-noto -y
-
-DEBIAN_FRONTEND=noninteractive apt install flatpak gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists --system flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-DEBIAN_FRONTEND=noninteractive apt install snapd gnome-software-plugin-snap -y
 snap install snapd
 
 dpkg -i /usr/share/debuntu/ubuntu-wallpapers-xenial.deb
 dpkg -i /usr/share/debuntu/ubuntu-wallpapers.deb
+echo -e "[org/gnome/desktop/interface]\nicon-theme='Papirus'" > /etc/gdm3/greeter.dconf-defaults
 
-(cd /tmp && curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg)
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
-apt update
-DEBIAN_FRONTEND=noninteractive apt install google-chrome-stable -y
+systemctl enable unattended-upgrades
 
 echo "" > /etc/network/interfaces
 DEBIAN_FRONTEND=noninteractive apt autoremove -y
